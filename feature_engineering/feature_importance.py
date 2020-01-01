d37afa81eac8c35ca56ab71d96fb723718dd5467
Jotaro <jojojoe77@gmail.com>
2020-01-01 16:56:29 +0900

    added feature importance

---
+++ feature_engineering/feature_importance.py
@@ -0,0 +1,10 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+# %matplotlib inline
+
+from xgboost import XGBRegressor
+from xgboost import plot_importance
+
+def plot_features(booster, figsize):    
+    fig, ax = plt.subplots(1,1,figsize=figsize)
+    return plot_importance(booster=booster, ax=ax)
